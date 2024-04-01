7c4fb91b0220165abd443726bb26bd2c648dac6b
b-ma <benjamin.matuszewski@gmail.com>
2024-04-01 08:40:39 +0200

    test script processor node

---
+++ README.md
@@ -0,0 +1,10 @@
+# test-script-offline
+
+Simple template for Web Audio demos and tests
+
+## Usage
+
+```
+cd path/to/dir
+npx serve
+```
---
+++ main.js
@@ -0,0 +1,79 @@
+import { html, render } from 'https://unpkg.com/lit-html';
+import 'https://unpkg.com/@ircam/sc-components@latest';
+
+import resumeAudioContext from './lib/resume-audio-context.js';
+
+let OFFLINE_TEST = true;
+
+function fib(n) {
+    let a = 0, b = 1, c, i;
+    if (n == 0) {
+      return a;
+    }
+    for (i = 2; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+}
+
+console.time('fib2000');
+fib(1000000);
+console.timeEnd('fib2000');
+
+
+const onlineAudioContext = new AudioContext();
+await resumeAudioContext(onlineAudioContext);
+render(html`ok`, document.body);
+
+const offlineAudioContext = new OfflineAudioContext(1, 48000, 48000);
+
+const audioContext = OFFLINE_TEST ? offlineAudioContext : onlineAudioContext;
+
+
+// audio chain
+const scriptProcessor = audioContext.createScriptProcessor(256, 1, 1);
+
+const freq = 300;
+const phaseIncr = 2 * Math.PI * freq / audioContext.sampleRate;
+const buffer = new Float32Array(256);
+let phase = 0;
+
+scriptProcessor.addEventListener('audioprocess', e => {
+  console.log('audioprocess');
+  // do some heavy work
+  fib(1000000);
+
+  const input = e.inputBuffer.getChannelData(0);
+  const output = e.outputBuffer.getChannelData(0);
+
+  for (let i = 0; i < buffer.length; i++) {
+    const v = Math.sin(phase);
+    phase += phaseIncr;
+
+    output[i] = input[i] * 0.5 + v * 0.5;
+  }
+});
+
+scriptProcessor.connect(audioContext.destination);
+
+const sine = audioContext.createOscillator();
+sine.frequency.value = 200;
+sine.connect(scriptProcessor);
+sine.start();
+
+if (OFFLINE_TEST) {
+  console.time('time to compute');
+  const output = await audioContext.startRendering();
+  console.timeEnd('time to compute');
+
+  console.log(output.getChannelData(0));
+  // playback
+  const src = onlineAudioContext.createBufferSource();
+  src.buffer = output;
+  src.connect(onlineAudioContext.destination);
+  src.start();
+}
+
+
